137d39730fae7b136b5c3177d518c1b619e9351c
zeusyosefgithub <midligyosef190@gmail.com>
2024-01-21 18:56:45 +0200

    starting -2

---
+++ app/FireBase/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHZ-9Z1eNkqPf-1jIqkNdWnDA90VAXehU",
+  authDomain: "mohamd-drag.firebaseapp.com",
+  projectId: "mohamd-drag",
+  storageBucket: "mohamd-drag.appspot.com",
+  messagingSenderId: "198459694421",
+  appId: "1:198459694421:web:578cdc5e85ca5dd00c63dc",
+  measurementId: "G-CEZ94TPJ2M"
+};
+
+const app = initializeApp(firebaseConfig);
